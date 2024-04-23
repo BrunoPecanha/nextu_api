651114f6c85727b409506140236d7a5ad240dcb4
Bruno Peçanha <brunoitdesenv@gmail.com>
2024-04-22 23:19:28 -0300

    Ajuste automapper

--- api/Config/MapperConfigProfile.cs
+++ api/Config/MapperConfigProfile.cs
@@ -8,25 +8,8 @@ namespace WeApi.Config
     {
         public MapperConfigProfile()
         {
-            CreateMap<CostumerDto, Costumer>()
-                .ForMember(x => x.Id, m => m.MapFrom(a => a.Id))
-                .ForMember(x => x.Name, m => m.MapFrom(a => a.Name))
-                .ForMember(x => x.LastName, m => m.MapFrom(a => a.LastName))
-                .ForMember(x => x.Phone, m => m.MapFrom(a => a.Phone))
-                .ForMember(x => x.Street, m => m.MapFrom(a => a.Street))
-                .ForMember(x => x.Number, m => m.MapFrom(a => a.Number))
-                .ForMember(x => x.City, m => m.MapFrom(a => a.City))
-                .ForMember(x => x.Active, m => m.MapFrom(a => a.Active));
-
-            CreateMap<Costumer, CostumerDto>()
-                .ForMember(x => x.Id, m => m.MapFrom(a => a.Id))
-                .ForMember(x => x.Name, m => m.MapFrom(a => a.Name))
-                .ForMember(x => x.LastName, m => m.MapFrom(a => a.LastName))
-                .ForMember(x => x.Phone, m => m.MapFrom(a => a.Phone))
-                .ForMember(x => x.Street, m => m.MapFrom(a => a.Street))
-                .ForMember(x => x.Number, m => m.MapFrom(a => a.Number))
-                .ForMember(x => x.City, m => m.MapFrom(a => a.City))
-                .ForMember(x => x.Active, m => m.MapFrom(a => a.Active));
+            CreateMap<CostumerDto, Costumer>();
+            CreateMap<Costumer, CostumerDto>();
         }
     }
 }
