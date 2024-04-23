bcf9beeb2d0a58e3c4cf46104e61831a5a67ea6b
Bruno Peçanha <brunoitdesenv@gmail.com>
2024-04-22 23:13:26 -0300

    Ajustes

--- supply.domain/ICostumerRepository.cs
+++ supply.domain/ICostumerRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace Supply.Domain
 {
     public interface ICostumerRepository : IRepositoryBase<Costumer> {
-        Task<Costumer> GetByIdAsync(int id);
-        Task<IEnumerable<Costumer>> GetAllAsync();       
+        public Task<Costumer> GetByIdAsync(int id);
+        public Task<IEnumerable<Costumer>> GetAllAsync();       
     }
 }
