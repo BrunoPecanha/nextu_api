63215a88c800b259792ca7ab940196397e19066a
Millenacost <millenacosta@id.uff.br>
2025-05-29 16:41:59 -0300

    🚧 Ajusta coluna de cpf para nullable, pois quando o usuário deletar a conta, o cpf vai ser setado para null para o caso de futuramente ele querer criar outra conta com o mesmo cpf.

--- UFF.Infra/Migrations/20250529193257_Altera_cpf_para_nullable_quando_deletar_vai_para_null.Designer.cs
+++ UFF.Infra/Migrations/20250529193257_Altera_cpf_para_nullable_quando_deletar_vai_para_null.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UFF.Infra.Context;
@@ -11,9 +12,11 @@ using UFF.Infra.Context;
 namespace UFF.Infra.Migrations
 {
     [DbContext(typeof(UffContext))]
-    partial class UffContextModelSnapshot : ModelSnapshot
+    [Migration("20250529193257_Altera_cpf_para_nullable_quando_deletar_vai_para_null")]
+    partial class Altera_cpf_para_nullable_quando_deletar_vai_para_null
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
