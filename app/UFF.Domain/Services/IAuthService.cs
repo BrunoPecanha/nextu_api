4300ed88ae9bb2ef1e93c31877890b21741b1bfa
Millenacost <millenacosta@id.uff.br>
2025-06-21 18:20:29 -0300

    🔒️ Implementa refresh token 🔒️ Habilita authorizes; 🔒️ Implementa middleware para requisições;

--- app/UFF.Domain/Services/IAuthService.cs
+++ app/UFF.Domain/Services/IAuthService.cs
@@ -1,10 +1,14 @@
 ﻿using System.Threading.Tasks;
+using UFF.Domain.Commands;
 using UFF.Domain.Entity;
 
-namespace UFF.Domain
+namespace UFF.Domain.Services
 {
-    public interface IAuthService  {
-        public Task<string> AuthSync(string userName, string password);
+    public interface IAuthService
+    {
+        Task<CommandResult> AuthSync(string email, string password);
+        Task<CommandResult> RefreshTokenAsync(string refreshToken);
+        Task<CommandResult> RevokeRefreshTokenAsync(string refreshToken);
         public string HashPassword(User user, string password);
         public bool VerifyPassword(User user, string providedPassword);
     }
